eb7cf9b1ea7a019a05eed241f2c185865ff37fff
mamooooo74 <mamooo.74@icloud.com>
2021-01-12 17:06:09 +0900

    set goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,8 +1,16 @@
 class GoalsController < ApplicationController
+  
+  before_action :move_to_root, only: [:show] 
+
+
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+
 
   def new
     @goal = Goal.new
-
   end
 
   def create
@@ -21,6 +29,11 @@ class GoalsController < ApplicationController
 
     def goal_params
       params.require(:goal).permit(:title, :content)
+    end
+
+    def move_to_root
+      goal = Goal.find_by(id: params[:id])
+      redirect_to root_path unless goal && current_user.id == goal.user_id
     end
 
 
